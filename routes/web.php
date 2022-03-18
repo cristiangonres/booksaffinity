e1026121a2a8e7f5919f9fee17b11fef8b625dc2
cristian <gonzalez.resina@gmail.com>
2022-03-18 20:50:37 +0100

    Creado modelo book y ruta para probar que devuelve datos de prueba de la base de datos en /testdb

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Book;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/testdb', function () {
+    $books = Book::all();
+    foreach($books as $book){
+        echo $book->title . " - " . $book->publi_date . "<br>";
+    }
+
 });
