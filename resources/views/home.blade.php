b52a1b5e654d0f9eba75f24a4b80ac751df19dc2
cristian <gonzalez.resina@gmail.com>
2022-03-24 19:32:20 +0100

    ultimos cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,87 @@
 
 @section('content')
 
-BLADE PARA HOME
 
+<div class="row">
 <?php
 
+$admin=true;
+
       foreach($books as $book){
-          echo $book->title . " - " . $book->publi_date . "<br>";
+
+
+          //echo $book->title . " - " . $book->publi_date . "<br>";
+
+          echo '<div class="m-4 p-2 shadow rounded border-bottom container border-top d-flex col-md-12 flex-shrink-0">
+    
+          <div id="position">'.$book->book_id.'</div>
+          <div class="container">
+              <div class="row justify-content-start">
+                  <div class="col-3 portada ">
+                      <a href="#"><img src="../img/portada.png" width="150" height="200" alt="portada"></a>
+                  </div>
+                  <div class="col-6">
+                      <ul class="list-unstyled">
+                          <li>Titulo: <a href="#" style="text-decoration:none" class="link-dark">'.$book->title.'</a></li>
+                          <li>Autor: <a href="#" style="text-decoration:none" class="link-dark">xxxxxx</a></li></li>
+                          <li>Fecha publicado: <a href="#" style="text-decoration:none" class="link-dark">'.$book->publi_date.'</a></li></li>
+                          <li>Pais: <a href="#" style="text-decoration:none" class="link-dark">xxxxxxx</a></li></li>
+                          <li>Generos:</li>
+                          <a href="#" style="text-decoration:none" class="link-info">xxxx</a>, <a href="#" style="text-decoration:none" class="link-info">xxxx</a>, <a href="#"style="text-decoration:none" class="link-info">xxxx</a>
+                          
+                      </ul>
+                  </div>
+                  <div class="col-3 ratings text-center align-bottom border-start">
+                     
+                          <span class="badge bg-primary">9,0</span>
+                          <span class="badge bg-primary">172.184</span>
+                      
+                  </div>
+              </div>
+          </div>
+      
+      </div>';
+
+
       }
 
 ?>
+
+
+<div class="m-4 p-2 shadow rounded border-bottom container border-top d-flex col-md-12 flex-shrink-0">
+    
+    <div id="position">1</div>
+    <div class="container">
+        <div class="row justify-content-start">
+            <div class="col-3 portada ">
+                <a href="#"><img src="../img/portada.png" width="150" height="200" alt="portada"></a>
+            </div>
+            <div class="col-6">
+                <ul class="list-unstyled">
+                    <li>Titulo: <a href="#" style="text-decoration:none" class="link-dark">xxxxxxx</a></li>
+                    <li>Autor: <a href="#" style="text-decoration:none" class="link-dark">xxxxxxx</a></li></li>
+                    <li>Año: <a href="#" style="text-decoration:none" class="link-dark">xxxx</a></li></li>
+                    <li>Pais: <a href="#" style="text-decoration:none" class="link-dark">xxxxxxx</a></li></li>
+                    <li>Generos:</li>
+                    <a href="#" style="text-decoration:none" class="link-info">xxxx</a>, <a href="#" style="text-decoration:none" class="link-info">xxxx</a>, <a href="#"style="text-decoration:none" class="link-info">xxxx</a>
+                    
+                </ul>
+            </div>
+            <div class="col-3 ratings text-center align-bottom border-start">
+               
+                    <span class="badge bg-primary">9,0</span>
+                    <span class="badge bg-primary">172.184</span>
+                
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+</div>
 
 
 
