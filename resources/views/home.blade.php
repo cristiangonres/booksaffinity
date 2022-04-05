ebb32995e257a6a992d4ee6445da5e7951f44ac7
cristian <gonzalez.resina@gmail.com>
2022-04-05 14:02:33 +0200

    Error corregido al calcular la media de puntuación en libros sin votos, error dividir por cero. ARREGLADO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,19 +15,19 @@
             echo '<div class="m-4 p-2 shadow rounded border-bottom container border-top d-flex col-md-12 flex-shrink-0">
 
 
-                                                          <div class="container">
-                                                              <div class="row justify-content-start">
-                                                                  <div class="col-3 portada ">
-                                                                      <a href="#"><img src="data:image/jpeg;base64,' .
+                                                                  <div class="container">
+                                                                      <div class="row justify-content-start">
+                                                                          <div class="col-3 portada ">
+                                                                              <a href="#"><img src="data:image/jpeg;base64,' .
                 base64_encode($book->cover) .
                 '" width="150" height="200" alt="portada"></a>
-                                                                  </div>
-                                                                  <div class="col-6">
-                                                                      <ul class="list-unstyled">
-                                                                          <li>Titulo: <a href="#" style="text-decoration:none" class="link-dark">' .
+                                                                          </div>
+                                                                          <div class="col-6">
+                                                                              <ul class="list-unstyled">
+                                                                                  <li>Titulo: <a href="#" style="text-decoration:none" class="link-dark">' .
                 $book->title .
                 '</a></li>
-                                                                          <li>Autor:';
+                                                                                  <li>Autor:';
             $nauth = count($book->authors);
             for ($i = 0; $i < $nauth; $i++) {
                 echo '<a href="#" style="text-decoration:none" class="link-dark"> ' . $book->authors[$i]['author_name'] . ' </a>';
@@ -36,13 +36,13 @@
                 }
             }
             echo '</li></li>
-                                                                          <li>Año: <a href="#" style="text-decoration:none" class="link-dark">' .
+                                                                                  <li>Año: <a href="#" style="text-decoration:none" class="link-dark">' .
                 $year .
                 '</a></li></li>
-                                                                          <li>Pais: <a href="#" style="text-decoration:none" class="link-dark">' .
+                                                                                  <li>Pais: <a href="#" style="text-decoration:none" class="link-dark">' .
                 $book->country['country_name'] .
                 '</a></li></li>
-                                                                          <li>Generos:';
+                                                                                  <li>Generos:';
             $ngen = count($book->genres);
             for ($i = 0; $i < $ngen; $i++) {
                 echo '<a href="#" style="text-decoration:none" class="link-info"> ' . $book->genres[$i]['genre_name'] . '</a>';
@@ -54,28 +54,28 @@
             echo '</li>
 
 
-                                                                      </ul>
-                                                                  </div>
-                                                                  <div class="col-3 ratings text-center align-bottom border-start">
+                                                                              </ul>
+                                                                          </div>
+                                                                          <div class="col-3 ratings text-center align-bottom border-start">
 
-                                                                          <span class="badge bg-primary"> Puntuación: ';
+                                                                                  <span class="badge bg-primary"> Puntuación: ';
             $nrate = count($book->accounts);
             $rate = 0;
             for ($i = 0; $i < $nrate; $i++) {
                 $rate += $book->accounts[$i]['pivot']['rate'];
-
-
             }
-            echo round( $rate / $nrate, 2). '</span>
-                                                                          <span class="badge bg-primary"> Votos: ' .
+            $avgrate = ($nrate>0 ? round($rate / $nrate, 2) : 0);
+            echo $avgrate .
+                '</span>
+                                                                                  <span class="badge bg-primary"> Votos: ' .
                 count($book->accounts) .
                 '</span>
 
+                                                                          </div>
+                                                                      </div>
                                                                   </div>
-                                                              </div>
-                                                          </div>
 
-                                                      </div>';
+                                                              </div>';
         }
 
         ?>
