00a48e118150dabcf03f2f07f35b4e99ec50aa54
rvrvlz <97094526+rvrvlz@users.noreply.github.com>
2022-04-25 22:24:36 +0200

    cambios de miguel

---
+++ resources/views/afterEditUser.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title', 'Sign Up')
+
+@section('content')
+
+<p>
+    <?php 
+        var_dump($idUser);
+        echo '</br>', $idUser, '</br>', $info;
+    ?> 
+</p>
+
+@endsection
