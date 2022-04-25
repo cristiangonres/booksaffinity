42215f1744670d40e8dfa0244df949e9c048af0d
rvrvlz <97094526+rvrvlz@users.noreply.github.com>
2022-04-25 22:25:56 +0200

    cambios de miguel

---
+++ resources/views/afterSubmitAuthor.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('title', 'Sign Up')
+
+@section('content')
+
+<p>
+    <?php 
+        //var_dump($userName);
+        echo $name, '<br>', $birthdate, '<br>', $birthDeath, '<br>', $imageContent, '<br>', 'ID País: ', $countryID, ' Nombre País: ', $country, '<br>', $description;
+        //foreach($userNameDB as $username){
+            //echo $username->username;
+        //}
+    ?> 
+</p>
+
+@endsection
